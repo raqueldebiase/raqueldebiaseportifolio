441d3ead635b6f293bea93769ef2265dd98e4e2e
Raquel De Biase <raqueldebiase@gmail.com>
2024-08-26 10:31:51 +0100

    add cover letter

--- src/app/components/molecules/NavLink.tsx
+++ src/app/components/molecules/NavLink.tsx
@@ -40,6 +40,7 @@ const NavLinks = () => {
           <Link href="#projects" onClick={toggleMenu}>projects</Link>
           <Link href="#contacts" onClick={toggleMenu}>contacts</Link>
           <Link href="https://github.com/raqueldebiase/" target="_blank" onClick={toggleMenu}>github</Link>
+          <Link href="/coverletter.pdf" download="RaquelDebiase_CV.pdf" onClick={toggleMenu}>Download CV</Link>
         </div>
       </nav>
     </>
